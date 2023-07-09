75460bdae8cf997a4c1bce0fb024b3286916fa1a
twisti <twisti@slne.dev>
2023-07-09 21:30:44 +0200

    refactor(dependency): updated dependencies

--- src/main/kotlin/dev/slne/surf/libreaddon/triggers/Triggers.kt
+++ src/main/kotlin/dev/slne/surf/libreaddon/triggers/Triggers.kt
@@ -3,10 +3,11 @@ package dev.slne.surf.libreaddon.triggers
 import com.willfp.libreforge.triggers.Triggers.register
 import dev.slne.surf.libreaddon.triggers.custom.TriggerDropXp
 import dev.slne.surf.libreaddon.triggers.custom.TriggerEndermanTarget
+import dev.slne.surf.libreaddon.triggers.custom.TriggerSchedule
 
 object Triggers {
     init {
-        // register(TriggerSchedule) /* Uncomment to enable */
+        register(TriggerSchedule) /* Uncomment to enable */
         register(TriggerEndermanTarget)
         register(TriggerDropXp)
     }
